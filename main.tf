e84a0f3794b85203e056d6fdab743be2b42317c3
nr <nrahimi.dev@gmail.com>
2024-11-27 18:11:32 +0100

    adjust region in providers.tf and add initial setting for a VPC with subnets, IG and routing table

--- main.tf
+++ main.tf
@@ -1,7 +1,65 @@
-# Create VPC
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16"
+# create VPC
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.0.0.0/22" # 1024 ip adresses
   tags = {
     Name = "deham19"
   }
+}
+
+# create public subnet in AZ-a
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.0.0.0/24" # 256 ip adresses  
+  availability_zone = "us-west-2a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public-subnet-1"
+  }
+}
+
+# create private subnet in AZ-a 
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.0.1.0/24" # 256 ip adresses
+  availability_zone = "us-west-2a"
+  tags = {
+    Name = "private-subnet-1"
+  }
+}
+
+# create internet gateway 
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "capstone-igw"
+  }
+}
+
+# public route table
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "public route table"
+  }
+}
+
+# private route table 
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "private route table"
+  }
+}
+
+# connect public route table to internet gateway 
+resource "aws_route" "public_route" {
+  route_table_id         = aws_route_table.public_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.internet_gateway.id
+}
+
+# connect route table to public subnet 
+resource "aws_route_table_association" "public_subnet_1_association" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_route_table.id
 }
--- providers.tf
+++ providers.tf
@@ -8,5 +8,5 @@ terraform {
 }
 
 provider "aws" {
-  region = "eu-west-2"
+  region = "us-west-2"
 }
