ace11848c2ef7377addb97d46437804837ea91f9
nr <nrahimi.dev@gmail.com>
2024-11-27 19:04:57 +0100

    add NAT and connect connect route with private subnet via private route table

--- vpc.tf
+++ vpc.tf
@@ -63,3 +63,26 @@ resource "aws_route_table_association" "public_subnet_1_association" {
   subnet_id      = aws_subnet.public_subnet_1.id
   route_table_id = aws_route_table.public_route_table.id
 }
+
+# create NAT gateway 
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_eip.elastic_ip.id
+  subnet_id     = aws_subnet.public_subnet_1.id
+  depends_on    = [aws_internet_gateway.internet_gateway]
+  tags = {
+    Name = "deham19-nat-gateway"
+  }
+}
+
+# connect private route table to NAT gateway  
+resource "aws_route" "private_route" {
+  route_table_id         = aws_route_table.private_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat_gateway.id
+}
+
+# connect route table to private subnet  
+resource "aws_route_table_association" "private_subnet_1_association" {
+  subnet_id      = aws_subnet.private_subnet_1.id
+  route_table_id = aws_route_table.private_route_table.id
+}
